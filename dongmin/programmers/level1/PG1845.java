67c29e72259fc1015bacb1cea028a424865929ca
ddmkim94 <ddmkim94@gmail.com>
2022-10-07 00:28:34 +0900

    [PG, 1845] 폰켓몬

--- dongmin/programmers/level1/PG1845.java
+++ dongmin/programmers/level1/PG1845.java
@@ -1,5 +1,22 @@
-package programmers;
+package programmers.level1;
+
+import java.util.HashSet;
+import java.util.Set;
 
 // 폰켓몬
 public class PG1845 {
+    public static void main(String[] args) {
+        int solution = solution(new int[]{3, 1, 2, 3});
+        System.out.println("solution = " + solution);
+    }
+
+    public static int solution(int[] nums) {
+        Set<Integer> pokemon = new HashSet<>();
+
+        for (int num : nums) {
+            pokemon.add(num);
+        }
+
+        return Math.min(pokemon.size(), nums.length / 2);
+    }
 }
