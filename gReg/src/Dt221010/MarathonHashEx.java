038c08332c9307c1eed2c21d7d2d737a2ba69d15
Greg <Gregorio3.kim@gmail.com>
2022-10-08 15:55:16 +0900

    마라톤 Hash 풀이 추가

--- gReg/src/Dt221010/MarathonHashEx.java
+++ gReg/src/Dt221010/MarathonHashEx.java
@@ -2,26 +2,35 @@ package Dt221010;
 
 import java.util.*;
 
-public class MarathonEx {
+public class MarathonHashEx {
 	
 	public String solution(String[] participant, String[] completion) {
 		String answer = "";
-
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		// 1. 참가자를 모두 hash table에 저장 - key:이름, value:명(이름 중복시 +1)  
 		for(int i=0; i<participant.length; i++) {
-			for(int j=0; j<completion.length; j++) {
-				if(participant[i].equals(completion[j])) {
-					participant[i] = "";
-					completion[j] = "";
-				}
+			if(hm.containsKey(participant[i])) {
+				hm.replace(participant[i], hm.get(participant[i]) +1);
+			}
+			else 
+				hm.put(participant[i], 1);
+		}
+		
+		// 2. 완주 한 사람을 map value에서 -1 씩 함   
+		for(int j=0; j<completion.length; j++) {
+			if(hm.containsKey(completion[j])) {
+				hm.replace(completion[j], hm.get(completion[j]) - 1);
 			}
 		}
 		
+		// 3. 0 이 아닌 사람은 완주를 못 한 사람 
 		for(int i=0; i<participant.length; i++) {
-			if(!participant[i].equals("")){
+			if(hm.get(participant[i]) > 0) {
 				answer = participant[i];
 				break;
 			}
 		}
+
 		return answer;
 	}
 	
@@ -29,8 +38,8 @@ public class MarathonEx {
 		// TODO Auto-generated method stub
 		String[] participant = { "leo", "kiki", "eden" };
 		String[] completion = { "kiki", "eden" };
-		MarathonEx sol = new MarathonEx();
-		System.out.println(sol.solution(participant, completion));
+		MarathonHashEx sol = new MarathonHashEx();
+		System.out.println("[HashMap] \r\n" + sol.solution(participant, completion));
 		
 		String[] p2 = { "marina", "josipa", "nikola", "vinko", "filipa" };
 		String[] c2 = { "josipa", "filipa", "marina", "nikola" };
