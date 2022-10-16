aa55a84cd5cdd5e287c023eaf5fb85f8dd939305
Churogrammer <m_sense1031@naver.com>
2022-10-17 00:34:09 +0900

    [PG12906] 같은 숫자는 싫어

--- daseul/programmers/level1/PG12906.java
+++ daseul/programmers/level1/PG12906.java
@@ -1,22 +1,30 @@
 package programmers.level1;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
 
 public class PG12906 {
 	public int[] solution(int []arr) {
 		Stack<Integer> stack = new Stack<>();
-		Queue<Integer> result = new LinkedList<>();
-		stack.push(10);
+		stack.push(-1);
 
 		for(int value : arr) {
-			if(value != stack.pop()) {
+			if(value != stack.peek()) {
+				if(stack.peek() < 0) {	stack.pop();	}
+				stack.push(value);
+			}
+		}
+		
+        return stack.stream().mapToInt(i->i).toArray();
+		/*
+		Queue<Integer> result = new LinkedList<>();
+		int prev = -1;
+		for(int value : arr) {
+			if(prev != value) {
+				prev = value;
 				result.add(value);
 			}
-			stack.push(value);
 		}
-
-        return result.stream().mapToInt(i->i).toArray();
+		*/
+       // return result.stream().mapToInt(i->i).toArray();
 	}
 }
