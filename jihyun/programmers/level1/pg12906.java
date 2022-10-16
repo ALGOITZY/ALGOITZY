c58018b28b8231b35f1df7d63146a7920be9a3c7
jihyunJang <wlgusghdks1@gmail.com>
2022-10-16 18:06:52 +0900

    [pg42578] 프로그래머스 같은 숫자는 싫어 문제

---
+++ jihyun/programmers/level1/pg12906.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.IntFunction;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12906
+public class pg12906 {
+    public int[] solution(int []arr) {
+        int target = -1;
+        List<Integer> list = new ArrayList<>();
+        for(int e : arr){
+            if(target != e) {
+                list.add(e);
+                target = e;
+            }
+        }
+
+        return list.stream().mapToInt(i->i).toArray();
+    }
+}
