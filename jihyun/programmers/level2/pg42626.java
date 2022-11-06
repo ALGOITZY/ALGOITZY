da966d640b92b5c33d83c809665e6753fc4baa6d
JH901 <wlgusghdks1@gmail.com>
2022-11-06 21:40:30 +0900

    [pg42626] 더 맵게

---
+++ jihyun/programmers/level2/pg42626.java
@@ -0,0 +1,28 @@
+import java.util.*;
+// https://school.programmers.co.kr/learn/courses/30/lessons/42626
+// 더 맵게
+class Solution {
+    public int solution(int[] scovilleList, int K) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();    
+        for(int scov : scovilleList) {
+            heap.offer(scov);
+        }
+        
+        int time = 0;
+        int scoville = 0;
+        int min1, min2;
+        while(heap.peek() <= K) {
+            if(heap.size() == 1) return -1;
+            time+=1;
+            
+            min1 = heap.poll();
+            min2 = heap.poll();
+            
+            scoville= min1+(min2*2);
+              
+            heap.offer(scoville);
+        }
+        
+        return time;
+    }
+}
