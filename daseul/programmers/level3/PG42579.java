af629992eb1332f3e01cf05b0880eda192b8c3d7
churogrammer <m_sense1031@naver.com>
2022-11-07 00:58:07 +0900

    [PG, 42579] 베스트앨범 수정

--- daseul/programmers/level3/PG42579.java
+++ daseul/programmers/level3/PG42579.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,8 +11,7 @@ public class PG42579 {
 	public int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
         Map<String, List<Integer>> genreSongIdMap = new HashMap<>();
-        Map<Integer, String> genreOrderMap = new HashMap<>();
-        Map<String, Integer> genreOrderIdMap = new HashMap<>();
+        Map<String, Integer> genreCountMap = new HashMap<>();
         List<Integer> resultList = new ArrayList<>();
 
         for(int idx = 0; idx < genres.length; idx++) {
@@ -28,46 +26,22 @@ public class PG42579 {
 
         	genreSongIdMap.put(genre, playList);
 
-        	Integer playCount = genreOrderIdMap.get(genre);
-        	if(playCount == null){
-        		playCount = 0;
-        	}
-        	genreOrderIdMap.put(genre, playCount + newPlayCount);
+        	Integer playCount = genreCountMap.get(genre) != null ? genreCountMap.get(genre) : 0;
+        	genreCountMap.put(genre, playCount + newPlayCount);
         }
 
-        int[] orderArr = new int[genreSongIdMap.size()];
-        /*
-        int count = 0;
-        for(String genre : genreSongIdMap.keySet()) {
-        	List<Integer> songIdList = genreSongIdMap.get(genre);
-        	genreOrderMap.put(songIdList.size(), genre);
-        	orderArr[count++] = songIdList.size();
-        }
-        */
         Map<String, Integer> sortedGenreOrderIdMap = new LinkedHashMap<>();
+        genreCountMap.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+        	.forEachOrdered(x -> sortedGenreOrderIdMap.put(x.getKey(), x.getValue()));
 
-
-        genreOrderIdMap.entrySet()
-  .stream()
-  .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-  .forEachOrdered(x -> sortedGenreOrderIdMap.put(x.getKey(), x.getValue()));
-
-
-        List<Map.Entry<String, Integer>> entryList = new LinkedList<>(genreOrderIdMap.entrySet());
-        entryList.sort(Map.Entry.comparingByValue());
-        //for(Map.Entry<String, Integer> entry : entryList){
         for(String genre : sortedGenreOrderIdMap.keySet()) {
-        	//String genre = entry.getKey();
        		List<Integer> playList = genreSongIdMap.get(genre);
-        	int[] playCountOrder = new int[playList.size()];
-       		int idx = 0;
 
        		int pickCount = playList.size() < 2 ? playList.size() : 2;
 
        		int[] playOrderArr = {0, 0};
        		int[] playSongCountArr = {0, 0};
        		for(Integer songId : playList) {
-       			//playCountOrder[idx++] = plays[songId];
        			if(plays[songId] > playSongCountArr[1]) {
        				if(plays[songId] > playSongCountArr[0]) {
        					playOrderArr[1] = playOrderArr[0];
@@ -78,40 +52,11 @@ public class PG42579 {
        					playOrderArr[1] = songId;
        					playSongCountArr[1] = plays[songId];
        				}
-
        			}
        		}
-
-        		//Arrays.sort(playCountOrder);
-        		//resultList.addAll(pickCountFromArray(playCountOrder, pickCount));
-        		resultList.addAll(pickCountFromArray(playOrderArr, pickCount));
-        }
-
-        /*
-        Arrays.sort(orderArr);
-        for(int genreCount : orderArr) {
-        	String genre = genreOrderMap.get(genreCount);
-        	List<Integer> playList = genreSongIdMap.get(genre);
-        	int[] playCountOrder = new int[playList.size()];
-        	int idx = 0;
-
-        	int pickCount = playList.size() < 2 ? playList.size() : 2;
-        	int[] playOrderArr = {0, 0};
-        	for(Integer songId : playList) {
-        		//playCountOrder[idx++] = plays[songId];
-        		if(plays[songId] > playOrderArr[0]) {
-       				playOrderArr[1] = playOrderArr[0];
-       				playOrderArr[0] = songId;
-        		}
-        	}
-
-        	//Arrays.sort(playCountOrder);
-        	//resultList.addAll(pickCountFromArray(playCountOrder, pickCount));
         	resultList.addAll(pickCountFromArray(playOrderArr, pickCount));
-
         }
 
-        */
         answer = resultList.stream().mapToInt(i -> i).toArray();
         return answer;
     }
