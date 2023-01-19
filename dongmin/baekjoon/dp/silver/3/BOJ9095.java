6c60c7a98405c1b9685525d49b5de1443ce5b7b2
ddmkim94 <ddmkim94@gmail.com>
2023-01-19 16:02:28 +0900

    [BOJ, 9095] 1, 2, 3 더하기(DP)

---
+++ dongmin/baekjoon/dp/silver/3/BOJ9095.java
@@ -0,0 +1,28 @@
+package algoitzy.week16;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 1로 만들기
+public class BOJ9095 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[11];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
